554440fe291e594b3def32ffcade58e2b1cdf81b
Quan <Hilda_quan@163.com>
2022-11-08 15:08:51 +0800

    03-deployment

--- 03-deployments/main.go
+++ 03-deployments/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +12,35 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// liveness()
+	badVersion()
+}
+
+func badVersion() {
+	http.HandleFunc("healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(500)
+		w.Write([]byte("Errror !"))
+	})
+
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
+}
+
+func liveness() {
+	start := time.Now()
+
+	//  /healthz 接口会在启动成功的 15s 内正常返回 200 状态码，在 15s 后，会一直返回 500 的状态码
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		duration := time.Since(start)
+		if duration.Seconds() > 15 {
+			w.WriteHeader(500)
+			w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+		} else {
+			w.WriteHeader(200)
+			w.Write([]byte("OK"))
+		}
+	})
+
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":3000", nil)
 }
